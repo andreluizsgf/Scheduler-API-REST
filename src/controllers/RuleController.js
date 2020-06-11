f42f254fcd7cbd82c92354928af1632ca4035ca5
andreluizsgf <andreluizsgf@gmail.com>
2020-06-11 19:52:00 -0300

    refactor: add try catch to all functions on RuleController; env: add /database on .gitignore

--- src/controllers/RuleController.js
+++ src/controllers/RuleController.js
@@ -6,59 +6,108 @@ const createHelper = require("../helpers/createHelper")
 
 module.exports = {
 
-    create(req, res) {
-        let rule = req.body;
-
-        fs.readFile(database, (err, data) => {
-            if (err) throw err;
-            let rules = JSON.parse(data);
-            const intervals = req.body.intervals
-            const newRuleId = createHelper.getNewRuleId(rules);
-
-            createHelper.validateRule(rules, intervals);
-            
-            rules[newRuleId] = rule;
-             
-            let ruleJson = JSON.stringify(rules, null, 2);
-
-            fs.writeFile(database, ruleJson, (err) => {
-                if (err) throw err;
-
-                return res.sendStatus(200);
+    index(req, res){
+        try {
+            const rules = JSON.parse(fs.readFileSync(database, 'utf8'));
+            return res.status(200).json({
+                message: "Rules successfully listed.",
+                status: true,
+                metadata: rules
             });
-    
-        });   
+        }
+        catch (err) {
+            return res.status(400).json({
+                message: "problems listing the rules.",
+                status: false,
+                error: err.message
+            });
+        }
+    },
+
+    create(req, res) {
+        try {
+            const rule = req.body;
+            
+            const rules = JSON.parse(fs.readFileSync(database, 'utf8'));
+            
+            const validatedIntervals = createHelper.validateRule(req.body, rules);
+            
+            if(validatedIntervals == undefined){
+                return res.status(400).json({
+                    message: "All intervals are already created.",
+                    status: true,
+                });
+            }
+                
+            rule.intervals = validatedIntervals;
+            
+            const max = createHelper.getNewRuleId(rules);
+
+            const novaRuleId = max + 1;
+
+            rules[novaRuleId] = rule;
+            const rulesTratadas = JSON.stringify(rules,null,4); // 2 || 4
+
+            fs.writeFileSync(database, rulesTratadas, 'utf8');
+            
+            return res.status(201).json({
+                message: "Rule successfully created.",
+                status: true,
+                metadata: rule
+            });
+        }
+        catch (err) {
+            return res.status(500).json({
+                message: "Problems creating the new rule.",
+                status: false,
+                error: err.message
+            });
+        }
     },
 
     delete(req, res){
         const id = req.body.id;
 
-        fs.readFile(database, (err, data) => {
-            if (err) throw err;
-            let rules = JSON.parse(data);
+        try {
+            const id = req.body.id;
+
+            if (!id) {
+                return res.status(400).json({
+                    message: "Please inform a valid id.",
+                    status: true
+                });
+            }
+
+            const rules = JSON.parse(fs.readFileSync(database, 'utf8'));
+
+            if (!rules[id]) {
+                return res.status(404).json({
+                    message: "No rule found, impossible to delete.",
+                    status: true
+                });
+            }
+
             delete rules[id];
-            let ruleJson = JSON.stringify(rules, null, 2);
 
-            fs.writeFile(database, ruleJson, (err) => {
-                if (err) throw err;
+            const rulesTratadas = JSON.stringify(rules,null,4);
 
-                return res.sendStatus(200);
-            });
-    
-        });   
-    },
-
-    index(req, res){
-        fs.readFile(database, (err, data) => {
-            if (err) throw err;
-
-            const rules = JSON.parse(data);
+            fs.writeFileSync(database, rulesTratadas, 'utf8');
             
-            return res.send(rules);
-
-        });   
+            return res.status(200).json({
+                message: "Rule successfully deleted",
+                status: true
+            });
+        }
+        catch (err) {
+            return res.status(500).json({
+                message: "Problems deleting a rule.",
+                status: false,
+                error: err.message
+            });
+        }
+        
     },
-
+    
     async available(req, res){
         try{
             const startDay = req.body.days[0],
@@ -71,8 +120,12 @@ module.exports = {
             const availableHours = helper.getAvailableHours(dates, allIntervals); 
             
             return res.send(availableHours);
-        } catch{
-            return res.sendStatus(500);
+        } catch(err){
+            return res.status(500).json({
+                message: "Problems listing available a times.",
+                status: false,
+                error: err.message
+            });
         }   
     },
 };
--- src/helpers/createHelper.js
+++ src/helpers/createHelper.js
@@ -1,30 +1,78 @@
 const moment = require("moment");
 
-const getNewRuleId = function(rules){
-    let lastId = Object.keys(rules)[Object.keys(rules).length - 1];
+const getDay = function(date){ 
+    return moment(date, "DD-MM-YYYY").format('dddd');
+}
 
-    if(lastId === undefined)
-        lastId = 0;
-        
-    return +lastId + 1;
+const getNewRuleId = function(rules){
+    const ids = Object.keys(rules);
+    //console.log();
+    if(ids.length){
+        const max = ids.reduce(function(a, b) {
+            return Math.max(a, b) || 0;
+        });
+
+        return +max;
+    }
+    else
+        return 0;
 } 
 
-const validateRule = function(rules, intervals){
-    rulesArray = Object.entries(rules);
+const validateRule = function(newRule, rules){
+    let rulesArray = Object.entries(rules);
+    const max = rulesArray.length;
+    let intervals = newRule.intervals;
     
-    intervals.forEach(interval => {
-        let time = [moment(`2016-12-06 ${interval.start}`), moment(`2016-12-06 ${interval.end}`)];
-        
-        rulesArray.forEach(rule => {
+    let days = newRule.date ? newRule.date : newRule.days;
+    let flag = newRule.date ? "date" : "days";
 
-            let intervals = rule.map(interval => {
-                
-                console.log(interval);
+    let intervalsChecked = intervals.map(interval => {
+        let intervalChecked = {};
+        intervalChecked.interval = interval;
+
+        rulesArray.forEach(rule => {
+            let rulesIntervals = Object.entries(rule[1].intervals);
+            const max = rule[1].intervals.length;
+            let check = false;
+
+            rulesIntervals.forEach(e => {
+                if((interval.start <= e[1].end)  &&  (interval.end >= e[1].start) && ((days == rule[1].date) || (rule[1].days.some(r=> days.indexOf(r) >= 0)) || (rule[1].days.includes(getDay(days))))){
+                    check = true;
+                }
+
+                intervalChecked.check = check;
+                /*
+                for (let index = 0; index < max; index++) {
+                    let check = false;
+                    if((interval.start <= e[1].end)  &&  (interval.end >= e[1].start) && ((days == rule[1].date) || (rule[1].days.some(r=> days.indexOf(r) >= 0)) || (rule[1].days.includes(getDay(days))))){
+                        check = true;
+                        intervalChecked.check = check;
+                        break;
+                    }
+
+                    intervalChecked.check = check;   
+                }*/
             });
-        })
+            
+        });
+        
+        if(!intervalChecked.check)
+            return interval;
+        else
+            return;
     });
 
-    return "oi";
+    return intervalsChecked.filter(function(interval) {return interval != null});
 };
 
 module.exports = { getNewRuleId, validateRule }
+
+/*
+
+const start = intervall.find(element => interval.start > intervall.start || interval.start < intervall.end)
+                const end = intervall.find(element => interval.end > intervall || interval.end < intervall.end)
+
+                
+
+
+*/
